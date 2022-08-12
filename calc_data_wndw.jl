3ade676fc6d7acece6861e9540d9d778a6ae28d4
Nolan Reilly <nr2489@log-2.hpc.nyu.edu>
2022-08-12 16:42:15 -0400

    Extensive bugfix

--- calc_data_wndw.jl
+++ calc_data_wndw.jl
@@ -1,26 +1,36 @@
 using NetCDF
 using FFTW
+#using Plots
+#gr()
 
-filename = "foo.nc4"
+filename = "/scratch/nr2489/riceWRF_3km/wrfout_d01_2016-08-02_00:15:00.nc"
 
 # Read in data
-u_raw = ncread(filename, "U")
-v_raw = ncread(filename, "V")
-w_raw = ncread(filename, "W")
+u_raw = ncread(filename, "U", start=[1,1,10,1], count=[-1,-1,1,1])[:,:,:,1]
+v_raw = ncread(filename, "V", start=[1,1,10,1], count=[-1,-1,1,1])[:,:,:,1]
+w_raw = ncread(filename, "W", start=[1,1,10,1], count=[-1,-1,2,1])[:,:,:,1]
+
+println("Finished reading in data")
 
 # Linearly interpolate onto shared non-staggered grid
-nx = shape(v_raw)[3]
-ny = shape(w_raw)[2]
-nz = shape(u_raw)[1]
-xx = (0.5/nx):1/nx:(1-0.5/nx)
+nx = size(v_raw)[1]
+ny = size(w_raw)[2]
+nz = size(u_raw)[3]
+n2x = div(nx,2)+1
+xx = reshape((0.5/nx):1/nx:(1-0.5/nx), (nx,1))
 xs = 0:1/nx:1
-yy = (0.5/ny):1/ny:(1-0.5/ny)
+yy = reshape((0.5/ny):1/ny:(1-0.5/ny), (1,ny))
 ys = 0:1/ny:1
 ll = (0.5/nz):1/nz:(1-0.5/nz)
 ls = 0:1/nz:1
-U = 0.5*(u_raw[:,:,2:nx]+u_raw[:,:,1:nx-1])
-V = 0.5*(v_raw[:,2:ny,:]+v_raw[:,1:ny-1,:])
-W = 0.5*(w_raw[2:nz,:,:]+w_raw[1:nz-1,:,:])
+U = 0.5*(u_raw[2:nx+1,:,:]+u_raw[1:nx,:,:])
+u_raw = nothing
+V = 0.5*(v_raw[:,2:ny+1,:]+v_raw[:,1:ny,:])
+v_raw = nothing
+W = 0.5*(w_raw[:,:,2:nz+1]+w_raw[:,:,1:nz])
+w_raw = nothing
+
+println("Finished interpolating data")
 
 # Windowing
 Lw = 50/nx      # 50 grid cells ~ 150km
@@ -33,61 +43,68 @@ function fw(x, L)
         return exp(-(L/x)^2*exp(-L/(L-x)))
     end
 end
-wx = fw.(xx,Lw) .* fw.(1.-xx,Lw)
-wy = fw.(yy,Lw) .* fw.(1.-yy,Lw)
+wx = fw.(xx,Lw) .* fw.(1 .- xx,Lw)
+wy = fw.(yy,Lw) .* fw.(1 .- yy,Lw)
+#println(size(U))
+#println(size(wx),size(wy))
 U .*= wx.*wy
 V .*= wx.*wy
 W .*= wx.*wy
 
+println("Finished windowing")
+
 # High-pass filter
 function high_pass!(f, Lx, Ly, Lhi)
     nz,ny,nx = size(f)
-    for x = 1:nx
-        for y = 1:ny/2  # Because rfft only stores half of the coefficients
+    for x = 1:div(nx,2) # Because rfft only stores half of the coefficients
+        for y = 1:ny
             for l = 1:nz
                 kx = mod(x+nx/2-1,nx)/Lx
                 ky = mod(y+ny/2-1,ny)/Ly
                 if sqrt(kx^2 + ky^2) < pi/Lhi
-                    f[l,y,x] = 0.0
+                    f[x,y,l] = 0.0
                 end
             end
         end
     end
 end
-uf = rfft(U,dims=(2,3))
-vf = rfft(V,dims=(2,3))
-wf = rfft(W,dims=(2,3))
+uf = rfft(U,(1,2))
+vf = rfft(V,(1,2))
+wf = rfft(W,(1,2))
 Lhi = 1000          # filter wavelength in km
 Lx = nx*3           # width of domain in x
 Ly = ny*3           # width of domain in y
-high_pass!(uc,Lx,Ly,Lhi)
-high_pass!(vc,Lx,Ly,Lhi)
-high_pass!(wc,Lx,Ly,Lhi)
-Uf = irfft(uf,dims=(2,3))
-Vf = irfft(vf,dims=(2,3))
-Wf = irfft(wf,dims=(2,3))
+high_pass!(uf,Lx,Ly,Lhi)
+high_pass!(vf,Lx,Ly,Lhi)
+high_pass!(wf,Lx,Ly,Lhi)
+Uf = irfft(uf,nx,(1,2))
+Vf = irfft(vf,nx,(1,2))
+Wf = irfft(wf,nx,(1,2))
+uf = vf = wf = nothing
+
+println("Finished high-pass filtering")
 
 # Calculate Reynolds stresses
 function low_pass!(f, Lx, Ly, Llo)
     nz,ny,nx = size(f)
-    for x = 1:nx
-        for y = 1:ny/2  # Because rfft only stores half of the coefficients
+    for x = 1:div(nx,2) # Because rfft only stores half of the coefficients
+        for y = 1:ny
             for l = 1:nz
                 kx = mod(x+nx/2-1,nx)/Lx
                 ky = mod(y+ny/2-1,ny)/Ly
                 if sqrt(kx^2 + ky^2) > pi/Llo
-                    f[l,y,x] = 0.0
+                    f[x,y,l] = 0.0
                 end
             end
         end
     end
 end
-uuf = rfft(U.*U,dims=(2,3))
-uvf = rfft(U.*V,dims=(2,3))
-uwf = rfft(U.*W,dims=(2,3))
-vvf = rfft(V.*V,dims=(2,3))
-vwf = rfft(V.*W,dims=(2,3))
-wwf = rfft(W.*W,dims=(2,3))
+uuf = rfft(Uf.*Uf,(1,2))
+uvf = rfft(Uf.*Vf,(1,2))
+uwf = rfft(Uf.*Wf,(1,2))
+vvf = rfft(Vf.*Vf,(1,2))
+vwf = rfft(Vf.*Wf,(1,2))
+wwf = rfft(Wf.*Wf,(1,2))
 Llo = 100           # filter wavelength in km
 low_pass!(uuf,Lx,Ly,Llo)
 low_pass!(uvf,Lx,Ly,Llo)
@@ -95,36 +112,45 @@ low_pass!(uwf,Lx,Ly,Llo)
 low_pass!(vvf,Lx,Ly,Llo)
 low_pass!(vwf,Lx,Ly,Llo)
 low_pass!(wwf,Lx,Ly,Llo)
-UU = irfft(uuf,dims=(2,3))
-UV = irfft(uvf,dims=(2,3))
-UW = irfft(uwf,dims=(2,3))
-VV = irfft(vvf,dims=(2,3))
-VW = irfft(vwf,dims=(2,3))
-WW = irfft(wwf,dims=(2,3))
+UU = irfft(uuf,nx,(1,2))
+UV = irfft(uvf,nx,(1,2))
+UW = irfft(uwf,nx,(1,2))
+VV = irfft(vvf,nx,(1,2))
+VW = irfft(vwf,nx,(1,2))
+WW = irfft(wwf,nx,(1,2))
+uuf = uvf = uwf = vvf = vwf = wwf = nothing
+
+println("Finished calculating Reynolds stresses")
 
 # Extra! Extra! Calculating divergence of Reynolds stress tensor
-T = ncread(filename, "T")
-P = ncread(filename, "PB")
-Φ_raw = ncread(filename, "PHB")
-Φ = 0.5*(Φ_raw[2:nz,:,:].+Φ_raw[1:nz-1,:,:])
+T = ncread(filename, "T", start=[1,1,10,1], count=[-1,-1,1,1])[:,:,:,1]
+P = ncread(filename, "PB", start=[1,1,10,1], count=[-1,-1,1,1])[:,:,:,1]
+Φ_raw = ncread(filename, "PHB", start=[1,1,10,1], count=[-1,-1,2,1])[:,:,:,1]
+Φ = 0.5*(Φ_raw[:,:,2:nz].+Φ_raw[:,:,1:nz-1])
+Φ_raw = nothing
 g = 9.81            # m/s^2
 mm = 29970          # kg/mol
 R = 8.315           # kg m^2/(s^2 mol K)
 k = R/(g*mm)
-Z = zeros((nz,ny,nx))
+Z = zeros((nx,ny,nz))
 # dz = k T/P dΦ
 for i = 2:nz
     # this is for debugging mostly
-    @. Z[i,:,:] = Z[i-1,:,:] + k*(T[i,:,:]+T[i-1,:,:])/(P[i,:,:]+P[i-1,:,:])*(Φ[i,:,:]-Φ[i-1,:,:])
+    @. Z[:,:,i] = Z[:,:,i-1] + k*(T[:,:,i]+T[:,:,i])/(P[:,:,i]+P[:,:,i-1])*(Φ[:,:,i]-Φ[:,:,i-1])
 end
 # d/dz = dl/dz d/dl = P/(k T) d/dΦ
 function dZ(f, T, P, Φ, k)
     nz,ny,nx = shape(f)
     out = zeros((nz,ny,nx))
     for i = 2:nz-1
-    @. out[i,:,:] = 1/k*P[i,:,:]/T[i,:,:]*(f[i+1,:,:]-f[i-1,:,:])/(Φ[i+1,:,:]-Φ[i-1,:,:])
+    @. out[:,:,i] = 1/k*P[:,:,i]/T[:,:,i]*(f[:,:,i+1]-f[:,:,i-1])/(Φ[:,:,i+1]-Φ[:,:,i-1])
     end
-    out[1,:,:] = 1/k*P[1,:,:]/T[1,:,:]*(f[2,:,:]-f[1,:,:])/(Φ[2,:,:]-Φ[1,:,:])
-    out[nz,:,:] = 1/k*P[nz,:,:]/T[nz,:,:]*(f[nz,:,:]-f[nz-1,:,:])/(Φ[nz,:,:]-Φ[nz-1,:,:])
+    out[:,:,1] = 1/k*P[:,:,1]/T[:,:,1]*(f[:,:,2]-f[:,:,1])/(Φ[:,:,2]-Φ[:,:,1])
+    out[:,:,nz] = 1/k*P[:,:,nz]/T[:,:,nz]*(f[:,:,nz]-f[:,:,nz-1])/(Φ[:,:,nz]-Φ[:,:,nz-1])
     return out
 end
+
+println("Finished calculating divergence of Reynolds stress tensor")
+
+# Write to file
+
