0e8b49e3259699abf8b6a554e54d69b15f421a02
Nolan Reilly <nr2489@log-1.hpc.nyu.edu>
2022-09-06 15:42:28 -0400

    Introductory elements of Z differentiation

--- calc_data_full.jl
+++ calc_data_full.jl
@@ -6,12 +6,13 @@ using FFTW
 filename = "/scratch/nr2489/riceWRF_3km/wrfout_d01_2016-08-02_00:15:00.nc"
 
 zb = 10 # z batch size
-zmax = size(ncread(filename,"U",start=[1,1,1,1],count=[1,1,-1,1]),3)
+zmax = size(ncread(filename,"U",start=[1,1,1,1],count=[1,1,-1,1]))[3]
 
-nx = size(v_raw)[1]
-ny = size(w_raw)[2]
-nz = size(u_raw)[3]
+nx = size(ncread(filename,"V",start=[1,1,1,1],count=[-1,1,1,1]))[1]
+ny = size(ncread(filename,"W",start=[1,1,1,1],count=[1,-1,1,1]))[2]
+nz = zb #size(ncread(filename,"U",start=[1,1,1,1],count=[1,1,-1,1]))[3]
 n2x = div(nx,2)+1
+n2y = div(ny,2)+1
 xx = reshape((0.5/nx):1/nx:(1-0.5/nx), (nx,1))
 xs = 0:1/nx:1
 yy = reshape((0.5/ny):1/ny:(1-0.5/ny), (1,ny))
@@ -27,6 +28,7 @@ function fw(x, L)
         return exp(-(L/x)^2*exp(-L/(L-x)))
     end
 end
+Lw = 50/nx      # 50 grid cells ~ 150km
 wx = fw.(xx,Lw) .* fw.(1 .- xx,Lw)
 wy = fw.(yy,Lw) .* fw.(1 .- yy,Lw)
 
@@ -63,6 +65,33 @@ function low_pass!(f, Lx, Ly, Llo)
     end
     f .= irfft(ff,nx,(1,2))
 end
+function ddx(f, dx)
+    # Spectrally differentiate f in terms of x
+    nx,ny,nz = size(f)
+    fh = rfft(f,(1,))
+    kk = [1im*mod(k-n2x,nx)+n2x for k=1:nx]
+    fh .*= kk
+    fh ./= dx
+    return irfft(fh,nx,(1,))
+end
+function ddy(f, dy)
+    # Spectrally differentiate f in terms of y
+    nx,ny,nz = size(f)
+    fh = rfft(f,(2,))
+    kk = [1im*mod(k-n2y,ny)+n2y for k=1:ny]
+    fh .*= kk
+    fh ./= dy
+    return irfft(fh,ny,(2,))
+end
+function ddz(f, z)
+    # Finite-difference approximation to the derivative df/dz, expressed in terms of a third indexing parameter
+    nx,ny,nz = size(f)
+    fh = zeros((nx,ny,nz))
+    for i = 2:nz-1
+        fh[:,:,i-1] .= (f[:,:,i+1] .- f[:,:,i-1])./(z[:,:,i+1] .- z[:,:,i-1])
+    end
+end
+
 fout = "dataslice.nc4"
 isfile(fout) && rm(fout)
 UUatts = Dict("longname" => "UU Reynolds flux", "units" => "m^2 s^-2")
@@ -74,15 +103,15 @@ WWatts = Dict("longname" => "WW Reynolds flux", "units" => "m^2 s^-2")
 Uatts  = Dict("longname" => "U gravity wave field", "units" => "m/s")
 Vatts  = Dict("longname" => "V gravity wave field", "units" => "m/s")
 Watts  = Dict("longname" => "W gravity wave field", "units" => "m/s")
-nccreate(fout,"UU","X",nx,"Y",ny,"L",nz,atts=UUatts)
-nccreate(fout,"UV","X",nx,"Y",ny,"L",nz,atts=UVatts)
-nccreate(fout,"UW","X",nx,"Y",ny,"L",nz,atts=UWatts)
-nccreate(fout,"VV","X",nx,"Y",ny,"L",nz,atts=VVatts)
-nccreate(fout,"VW","X",nx,"Y",ny,"L",nz,atts=VWatts)
-nccreate(fout,"WW","X",nx,"Y",ny,"L",nz,atts=WWatts)
-nccreate(fout,"U","X",nx,"Y",ny,"L",nz,atts=Uatts)
-nccreate(fout,"V","X",nx,"Y",ny,"L",nz,atts=Vatts)
-nccreate(fout,"W","X",nx,"Y",ny,"L",nz,atts=Watts)
+nccreate(fout,"UU","X",nx,"Y",ny,"L",zmax,atts=UUatts)
+nccreate(fout,"UV","X",nx,"Y",ny,"L",zmax,atts=UVatts)
+nccreate(fout,"UW","X",nx,"Y",ny,"L",zmax,atts=UWatts)
+nccreate(fout,"VV","X",nx,"Y",ny,"L",zmax,atts=VVatts)
+nccreate(fout,"VW","X",nx,"Y",ny,"L",zmax,atts=VWatts)
+nccreate(fout,"WW","X",nx,"Y",ny,"L",zmax,atts=WWatts)
+nccreate(fout,"U","X",nx,"Y",ny,"L",zmax,atts=Uatts)
+nccreate(fout,"V","X",nx,"Y",ny,"L",zmax,atts=Vatts)
+nccreate(fout,"W","X",nx,"Y",ny,"L",zmax,atts=Watts)
 
 for zm = 1:zb:zmax
 
@@ -90,6 +119,7 @@ for zm = 1:zb:zmax
     u_raw = ncread(filename, "U", start=[1,1,zm,1], count=[-1,-1,zb,1])[:,:,:,1]
     v_raw = ncread(filename, "V", start=[1,1,zm,1], count=[-1,-1,zb,1])[:,:,:,1]
     w_raw = ncread(filename, "W", start=[1,1,zm,1], count=[-1,-1,zb+1,1])[:,:,:,1]
+    z_raw = ncread(filename, "PHB", start=[1,1,zm,1], count=[-1,-1,zb+1,1])[:,:,:,1]
 
     #println("Finished reading in data")
 
@@ -100,11 +130,12 @@ for zm = 1:zb:zmax
     v_raw = nothing
     W = 0.5*(w_raw[:,:,2:nz+1]+w_raw[:,:,1:nz])
     w_raw = nothing
+    Z = 0.5*(z_raw[:,:,2:nz+1]+z_raw[:,:,1:nz])
+    z_raw = nothing
 
     #println("Finished interpolating data")
 
     # Windowing
-    Lw = 50/nx      # 50 grid cells ~ 150km
     U .*= wx.*wy
     V .*= wx.*wy
     W .*= wx.*wy
@@ -141,44 +172,22 @@ for zm = 1:zb:zmax
     #println("Finished calculating Reynolds stresses")
 
     # Extra! Extra! Calculating divergence of Reynolds stress tensor
-    T = ncread(filename, "T", start=[1,1,zm,1], count=[-1,-1,zb,1])[:,:,:,1]
-    P = ncread(filename, "PB", start=[1,1,zm,1], count=[-1,-1,zb,1])[:,:,:,1]
-    Φ_raw = ncread(filename, "PHB", start=[1,1,zm,1], count=[-1,-1,zb+1,1])[:,:,:,1]
-    Φ = 0.5*(Φ_raw[:,:,2:nz].+Φ_raw[:,:,1:nz-1])
-    Φ_raw = nothing
-    g = 9.81            # m/s^2
-    mm = 29970          # kg/mol
-    R = 8.315           # kg m^2/(s^2 mol K)
-    k = R/(g*mm)
-    Z = zeros((nx,ny,nz))
-    # dz = k T/P dΦ
-    for i = 2:nz
-        # this is for debugging mostly
-        @. Z[:,:,i] = Z[:,:,i-1] + k*(T[:,:,i]+T[:,:,i])/(P[:,:,i]+P[:,:,i-1])*(Φ[:,:,i]-Φ[:,:,i-1])
-    end
-    # d/dz = dl/dz d/dl = P/(k T) d/dΦ
-    function dZ(f, T, P, Φ, k)
-        nz,ny,nx = shape(f)
-        out = zeros((nz,ny,nx))
-        for i = 2:nz-1
-        @. out[:,:,i] = 1/k*P[:,:,i]/T[:,:,i]*(f[:,:,i+1]-f[:,:,i-1])/(Φ[:,:,i+1]-Φ[:,:,i-1])
-        end
-        out[:,:,1] = 1/k*P[:,:,1]/T[:,:,1]*(f[:,:,2]-f[:,:,1])/(Φ[:,:,2]-Φ[:,:,1])
-        out[:,:,nz] = 1/k*P[:,:,nz]/T[:,:,nz]*(f[:,:,nz]-f[:,:,nz-1])/(Φ[:,:,nz]-Φ[:,:,nz-1])
-        return out
-    end
-
-    println("Finished calculating divergence of Reynolds stress tensor")
+    Dx = ddx(UU,dx) .+ ddy(UV,dy) .+ ddz(UW,Z)
+    Dy = ddx(UV,dx) .+ ddy(VV,dy) .+ ddz(VW,Z)
+    Dz = ddx(UW,dx) .+ ddy(VW,dy) .+ ddz(WW,Z)
+    
+    #println("Finished calculating divergence of Reynolds stress tensor")
 
     # Write to file
-    ncwrite(UU,fout,"UU", start=[1,1,zm,1], count=[-1,-1,zb,1])
-    ncwrite(UV,fout,"UV", start=[1,1,zm,1], count=[-1,-1,zb,1])
-    ncwrite(UW,fout,"UW", start=[1,1,zm,1], count=[-1,-1,zb,1])
-    ncwrite(VV,fout,"VV", start=[1,1,zm,1], count=[-1,-1,zb,1])
-    ncwrite(VW,fout,"VW", start=[1,1,zm,1], count=[-1,-1,zb,1])
-    ncwrite(WW,fout,"WW", start=[1,1,zm,1], count=[-1,-1,zb,1])
-    ncwrite(uf,fout,"U", start=[1,1,zm,1], count=[-1,-1,zb,1])
-    ncwrite(vf,fout,"V", start=[1,1,zm,1], count=[-1,-1,zb,1])
-    ncwrite(vf,fout,"W", start=[1,1,zm,1], count=[-1,-1,zb,1])
+println(zm)
+    ncwrite(UU,fout,"UU", start=[1,1,zm], count=[-1,-1,zb])
+    ncwrite(UV,fout,"UV", start=[1,1,zm], count=[-1,-1,zb])
+    ncwrite(UW,fout,"UW", start=[1,1,zm], count=[-1,-1,zb])
+    ncwrite(VV,fout,"VV", start=[1,1,zm], count=[-1,-1,zb])
+    ncwrite(VW,fout,"VW", start=[1,1,zm], count=[-1,-1,zb])
+    ncwrite(WW,fout,"WW", start=[1,1,zm], count=[-1,-1,zb])
+    ncwrite(uf,fout,"U", start=[1,1,zm], count=[-1,-1,zb])
+    ncwrite(vf,fout,"V", start=[1,1,zm], count=[-1,-1,zb])
+    ncwrite(vf,fout,"W", start=[1,1,zm], count=[-1,-1,zb])
     ncclose(fout)
 end
